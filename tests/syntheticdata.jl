997d530fd846fcf2af363c9929907fee772479c5
Nicholas Richardson <njericha@uwaterloo.ca>
2023-11-10 17:03:34 -0800

    add plots to synthetic data

--- tests/syntheticdata.jl
+++ tests/syntheticdata.jl
@@ -3,10 +3,15 @@
 using Random
 using KernelDensity
 using MatrixTensorFactor
+using Plots
 
 Random.seed!(123)
 
-N = 5 # number of samples
+##########
+#   1D   #
+##########
+
+N = 10 # number of samples
 M = 3 # features
 
 list_of_samples = [randn(M) for _ in 1:N]
@@ -17,9 +22,20 @@ kde(list_of_measurements[1])
 
 kdes = make_densities(list_of_samples)
 
-##########################
+f = kdes[1] # the first estimated kernel
+
+p = plot(f.x, f.density)
+display(p)
+
+##########
+#   2D   #
+##########
 
 locations = [randn(N, 2) for _ in 1:M]
 
 
 kdes = make_densities(locations)
+f = kdes[1] # the first estimated kernel
+
+p = plot(f.x, f.y, f.density)
+display(p)
