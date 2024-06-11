9c220c2cdc13138f6c1decab21d128b1264693bf
Nicholas <njericha@math.ubc.ca>
2024-06-11 11:03:32 -0700

    add ProjectedNormalization

--- src/constraint.jl
+++ src/constraint.jl
@@ -6,24 +6,31 @@ Low level code for the types of constraints
 abstract type AbstractConstraint <: Function end
 
 """
+    check(C::AbstractConstraint, A::AbstractArray)::Bool
+
+Returns `true` if `A` satisfies the constraint `C`.
+"""
+function check(_::AbstractConstraint, _::AbstractArray); end
+
+"""
     GenericConstraint <: AbstractConstraint
 
 General constraint. Simply applies apply and checks with check. Composing any two
 `AbstractConstraint`s will return this type.
 
-Calling a `GenericConstraint` on an `AbstractDecomposition` will use the function in the
-field `apply`. Use `check(A, C::GenericConstraint)` to use the function in the field `check`.
+Calling a `GenericConstraint` on an `AbstractArray` will use the function in the
+field `apply`. Use `check(C::GenericConstraint, A)` to use the function in the field `check`.
 """
 struct GenericConstraint <: AbstractConstraint
-    apply::Function # input a AbstractDecomposition -> mutate it so that `check` would return true
+    apply::Function # input a AbstractArray -> mutate it so that `check` would return true
     check::Function
 end
 
-function (C::GenericConstraint)(D::AbstractDecomposition)
+function (C::GenericConstraint)(D::AbstractArray)
     (C.apply)(D)
 end
 
-check(A::AbstractDecomposition, C::GenericConstraint) = (C.check)(A)
+check(C::GenericConstraint, A::AbstractArray) = (C.check)(A)
 
 # TODO should I be overloading the composition function like this?
 ∘(f::AbstractConstraint, g::AbstractConstraint) = GenericConstraint(f.apply ∘ g.apply, bool_function_and(f.check, g.check))
@@ -42,18 +49,27 @@ bool_function_and(f::Function, g::Function) = BoolFunctionAnd(f, g)
 abstract type AbstractNormalization <: AbstractConstraint end
 
 """
+    ProjectedNormalization(projection, norm; whats_normalized=identity)
 
+Main constructor for the constraint where `norm` of `whats_normalized` equals `scale`.
+
+Scale can be a single `Real`, or an `AbstractArray{<:Real}`, but should be the same size as
+the output of `whats_normalized`.
 """
 struct ProjectedNormalization <: AbstractNormalization
-    apply::Function # input a AbstractDecomposition -> mutate it so that `check` would return true
-    check::Function # input a AbstractDecomposition -> output a Bool
+    norm::Function # input a AbstractArray -> output a Bool
+    projection::Function # input a AbstractArray -> mutate it so that `check` would return true
+    whats_normalized::Function
 end
 
-struct ScaledNormalization{T<:Union{Real,AbstractArray{<:Real}}} <: AbstractNormalization
-    norm::Function
-    whats_normalized::Function
-    scale::T
+ProjectedNormalization(norm, projection; whats_normalized=identity)=ProjectedNormalization(norm, projection, whats_normalized)
+
+function (P::ProjectedNormalization)(A::AbstractArray)
+    whats_normalized_A = P.whats_normalized(A)
+    (P.projection).(whats_normalized_A)
 end
+
+check(P::ProjectedNormalization, A::AbstractArray) = all((P.norm).(P.whats_normalized(A)) .== 1)
 
 """
     ScaledNormalization(norm; whats_normalized=identity, scale=1)
@@ -63,16 +79,22 @@ Main constructor for the constraint where `norm` of `whats_normalized` equals `s
 Scale can be a single `Real`, or an `AbstractArray{<:Real}`, but should be the same size as
 the output of `whats_normalized`.
 """
+struct ScaledNormalization{T<:Union{Real,AbstractArray{<:Real}}} <: AbstractNormalization
+    norm::Function
+    whats_normalized::Function
+    scale::T
+end
+
 ScaledNormalization(norm;whats_normalized=identity,scale=1)=ScaledNormalization{typeof(scale)}(norm, whats_normalized, scale)
 
-function (S::ScaledNormalization)(A::AbstractDecomposition)
+function (S::ScaledNormalization)(A::AbstractArray)
     whats_normalized_A = S.whats_normalized(A)
     A_norm = (S.norm).(whats_normalized_A) ./ S.scale(A)
     whats_normalized_A ./= A_norm
     return A_norm
 end
 
-check(A::AbstractDecomposition, S::ScaledNormalization) = all((S.norm).(S.whats_normalized(A)) .== S.scale)
+check(S::ScaledNormalization, A::AbstractArray) = all((S.norm).(S.whats_normalized(A)) .== S.scale)
 
 """Entrywise constraint. Note both apply and check needs to be performed entrywise on an array"""
 struct EntryWise <: AbstractConstraint
@@ -86,10 +108,10 @@ function (C::EntryWise)(A::AbstractArray)
 end
 
 """
-    check(A::AbstractArray, C::EntryWise)::Bool
+    check(C::EntryWise, A::AbstractArray)::Bool
 
 Checks if `A` is entrywise constrained
 """
-check(A::AbstractArray, C::EntryWise) = all((C.check).(A))
+check(C::EntryWise, A::AbstractArray) = all((C.check).(A))
 
 const nnegative! = EntryWise(x -> max(0, x), x -> x >= 0)
