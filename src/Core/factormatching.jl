69c084c1492985960fe12690a8fe854ea1c38eda
Nicholas <njericha@math.ubc.ca>
2025-04-28 10:10:17 -0700

    more stable backup algorithm for match_slices!

--- src/Core/factormatching.jl
+++ src/Core/factormatching.jl
@@ -18,15 +18,29 @@ function match_slices!(X::AbstractArray, Y::AbstractArray; dims, dist=L2)
     if !allunique(ordering)
         # Global optimal order would involve checking factorial(n_slices) options
         # Use a greedy approach instead
-        @warn "`match_slices!` may not have found the best ordering; using greedy approach."
-        ordering = zeros(Int, n_slices)
-        X_slices = collect(enumerate(eachslice(X; dims))) # [(1, X1), (2, X2), ...]
+        @warn "`match_slices!` may not have found the best ordering; using O(n²) greedy approach."
+
+        dist_matrix = zeros(n_slices, n_slices)
         for (j, Yj) ∈ enumerate(eachslice(Y; dims))
-            _, idx = findmin(j_Xj -> dist(j_Xj[2], Yj), X_slices)
-            j_match = X_slices[idx][1] # index of the slice: X_slices[idx] == (j, Xj)
-            ordering[j] = j_match
-            popat!(X_slices, idx) # remove slice Xj from the options
+            for (i, Xi) ∈ enumerate(eachslice(X; dims))
+                dist_matrix[i, j] = dist(Xi, Yj)
+            end
         end
+
+        # Find the closest slices (Xi, Yj), remove the from the options, and repeat.
+        # We don't saved a sliced dist_matrix since that would renumber the rows/cols.
+        # Instead fill the row/col with Inf so argmin can no longer pick that row/col.
+        pairings = zeros(CartesianIndex{2}, n_slices)
+        for n in 1:n_slices
+            pairing = argmin(dist_matrix) # CartesianIndex(i, j)
+            dist_matrix[pairing[1], :] .= Inf # remove row i
+            dist_matrix[:, pairing[2]] .= Inf # remove col j
+            pairings[n] = pairing
+        end
+
+        sort!(pairings) # Sorts by second index (Yj)
+
+        ordering = map(x->x[1], pairings) # Ordering is given by the first index (Xi)
     end
 
     # Permute slice of X
